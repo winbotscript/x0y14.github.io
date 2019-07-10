5cd14bb71ba5ae9c7572934db68e9a9ddcdb890d
x0y14 <ya01ma08@gmail.com>
2019-07-10 15:17:18 +0900

    small fix

--- liffer.js
+++ liffer.js
@@ -54,7 +54,7 @@ function initializeApp(data) {
             "imageUrl": "https://scontent-lga3-1.cdninstagram.com/vp/58a09f9b4c9b5c1e2ab74eacca9f4265/5DBBFF72/t51.2885-15/e35/s1080x1080/64425074_344564302889595_2131522129282698551_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com",
             "action": {
                 "type": "uri",
-                "uri": "https://line.me/R/nv/postback?text=hello"
+                "uri": "line://nv/profilePopup/mid=u69a03e8b386bd9c8c2aa8f382ab1334d"
             }
         }]
     }
